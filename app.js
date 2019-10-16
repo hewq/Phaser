ad598f1c082b2b8fea0d31cc75ad8e31b2c4e844
wenqiu.he <wenqiu.he@yahoo.com>
2019-10-16 18:48:09 +0800

    u

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static('./apps/'));
+app.use('/demo', express.static('./demo/'));
 
 app.listen(8081, function () {
 	console.log('Phaser is running at port 8081...');
