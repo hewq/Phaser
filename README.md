1c1442d993a72713453ec931b3a43cf75643dcf2
何文秋 <hewenqiu@corp.netease.com>
2020-11-03 10:45:04 +0800

    chore:添加IP

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Phaser
-这是关于 Phaser3.x 的一些小 demo 
+# PHASER
 
-如果要运行我们的脚本，则需要安装Node.js。
-1. 下载代码，然后执行 `npm install` 安装依赖
-2. 在项目根目录下执行 `node app.js` 运行 `app.js` 文件启动本地服务器
-3. 在浏览器上通过 `localhost:8081/xxx/index.html` 浏览页面。`xxx` 对应 `apps` 目录下的文件夹名称，如 `a20190919firstgame`
+## 本地预览
+
+- 执行脚本 `npm run dev`
+- 在浏览器上通过 `localhost:8088/xxx/index.html` 浏览页面。`xxx` 对应 `apps` 目录下的文件夹名称，如 `a20190919firstgame`
--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-var express = require('express');
+let express = require('express');
 
-var app = express();
+let app = express();
 
 app.use('/', express.static('./apps/'));
 app.use('/demo', express.static('./demo/'));
 app.use('/public', express.static('./public/'));
 
-app.listen(8081, function() {
-    console.log('Phaser is running at port 8081...');
+function getIPAddress() {
+    let interfaces = require('os').networkInterfaces();
+    for (let devName in interfaces) {
+        let iface = interfaces[devName];
+        for (let i = 0; i < iface.length; i++) {
+            let alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+}
+
+const LOCAL_IP = getIPAddress();
+
+app.listen(8088, function() {
+    console.log(`http://${LOCAL_IP}:8088`);
 });
