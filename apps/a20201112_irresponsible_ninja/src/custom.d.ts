a18218e8fecda924609d2b6df22d3396e4b402e4
何文秋 <hewenqiu@corp.netease.com>
2020-11-22 21:31:31 +0800

    fix

--- apps/a20201112_irresponsible_ninja/src/custom.d.ts
+++ apps/a20201112_irresponsible_ninja/src/custom.d.ts
@@ -1,18 +1,18 @@
 declare module "*.png";
 
 declare interface FootConfig {
-    circleNum: number,
-    blueCircleFrame: number,
-    whileCircleFrame: number,
-    blueCloudY: number
-    whileCloudY: number,
-    height: number
+    circleNum: number;
+    blueCircleFrame: number;
+    whileCircleFrame: number;
+    blueCloudY: number;
+    whileCloudY: number;
+    height: number;
 }
 
 declare interface GameConfig {
-    processLen: number,
-    processHeight: number,
-    platformHeight: number,
-    stickWidth: number,
-    stickHeight: number
+    processLen: number;
+    processHeight: number;
+    platformHeight: number;
+    stickWidth: number;
+    stickHeight: number;
 }
